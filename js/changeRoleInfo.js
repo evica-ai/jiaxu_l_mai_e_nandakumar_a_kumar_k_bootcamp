c56b7fc717d3e1693c65e99d9357cbbd3d43dbe7
ashwin <ashu61998@gmail.com>
2021-09-16 09:23:45 +0530

    added ashwin bio

--- js/changeRoleInfo.js
+++ js/changeRoleInfo.js
@@ -1,5 +1,9 @@
 var person = [{
-
+    {
+        name: "Ashwin Nandakumar",
+        role: "Squad Leader",
+        description: "Ashwin is an front-end developer and does a bit of designing as well. He developed an liking towards html, css and js as he likes coding a lot. He took some interest towards designing as it excited him too, so he felt why not give it a try. Being an generalist, he works to bring a balance to the design and code workflow in a teamd animation designer. His strength lies in his ability to use his multidisciplinary skills in interaction design, 3D animation and user interface to create purposeful and aesthetically pleasing designs/experiences to help local companies create their brands."
+    },
 {
 name: "Kanak Kumar",
 role: "Design Specialist",
